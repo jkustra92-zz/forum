0280ce44cd09e72b3cc28360d4a5ad4cd90d6651
Jessica <jkustra92@gmail.com>
2016-10-02 18:57:14 -0400

    getting all posts to display on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
-  def index               #need a home controller/index for devise and my root to go to. 
+  def index               #need a home controller/index for devise and my root to go to.
+    @posts = Post.all
+    puts "==========="
+    puts @posts
+    puts "==========="
+
+    @posts.each do |post|
+      puts post.user
+    end
   end
 
   #eventually, get all the posts out of the database and display them here using the homepage view.
